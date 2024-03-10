623e1075aaba4c9dd67f07bbbccf3d8d005a0f5b
yuXuan <1730684217@qq.com>
2024-03-10 09:42:05 +0800

    add Element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup></script>
 
 <template>
-  <div>我的APP</div>
+  <div>
+    我的APP
+    <button @click="$route.push('/home')">跳首页</button>
+    <button>跳列表页</button>
+  </div>
 </template>
 
 <style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
+// createRouter用于创建路由实例
+// 配置history模式
+//vite中的环境变量import.meta.env.BASE_URL  就是vite.config.js中的base配置项
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: []
